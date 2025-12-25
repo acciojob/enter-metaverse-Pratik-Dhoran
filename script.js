c739789410e762e897f8c90d6f7790d70e2cd812
Pratik Dhoran <148314312+Pratik-Dhoran@users.noreply.github.com>
2025-12-25 11:24:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const btn = document.getElementById("enterBtn") ;
+const status = document.getElementById("status") ;
+
+btn.addEventListener("click" , function()
+					{
+			status.outerHTML = "<h1>Entered Metaverse</h1>";			
+					}) ;
